f919894a72c6964e093df23adbe3ef06d44ae2fc
Sergey Slobodenyuk <sergn75@gmail.com>
2023-05-15 10:27:41 +0200

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Sergey Slobodenyuk on 2023-05-15.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
